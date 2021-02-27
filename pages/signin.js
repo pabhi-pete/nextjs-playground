5138e1285516f89b8a3b487279af1a1255380dc2
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2021-02-27 14:32:17 -0800

    update material-ui config

--- pages/signin.js
+++ pages/signin.js
@@ -31,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
     height: '100vh',
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundImage: 'url(/signin.jpeg)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
